79df330eccc57bae25cbc155622dca1bdd3a7f07
sayyidkhan <sayyidkhan92@hotmail.com>
2024-12-04 00:14:19 +0800

    resize the window

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,22 +1,25 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MAP_CONFIG } from '../constants/data';
 
 function MyMap() {
   return (
-    <MapContainer 
-      center={[51.505, -0.09]}
-      zoom={13}
-      style={{ height: "400px", width: "100%" }}
-      attributionControl={false}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div style={{ width: '100%', height: '100%' }}>
+      <MapContainer 
+        center={MAP_CONFIG.DEFAULT_CENTER}
+        zoom={MAP_CONFIG.DEFAULT_ZOOM}
+        style={{ height: "100vh", width: "100%" }}
+        attributionControl={false}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={MAP_CONFIG.DEFAULT_CENTER}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
---
+++ src/constants/data.js
@@ -0,0 +1,20 @@
+// This file contains all the hardcoded data used throughout the application
+
+// Example format - modify according to your needs
+export const APP_CONFIG = {
+    APP_NAME: 'SunnySideUp',
+    VERSION: '1.0.0',
+};
+
+// Map related constants
+export const MAP_CONFIG = {
+    DEFAULT_CENTER: [1.3521, 103.8198], // Singapore coordinates
+    DEFAULT_ZOOM: 12,
+    MIN_ZOOM: 3,
+    MAX_ZOOM: 18,
+};
+
+// Add more constants as needed
+// export const YOUR_CONSTANT = {
+//     // your data here
+// };
