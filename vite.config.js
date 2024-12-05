92ca8342f531608c36185cfd6521bbb0e6817af8
sayyidkhan <sayyidkhan92@hotmail.com>
2024-12-05 23:55:50 +0800

    fix vercel

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        },
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
+          },
+          assetFileNames: 'assets/[name].[hash][extname]',
+          chunkFileNames: 'assets/[name].[hash].js',
+          entryFileNames: 'assets/[name].[hash].js'
+        }
       }
-    }
-  },
-  server: {
-    port: 3000
-  },
-  base: '/'
+    },
+    server: {
+      port: 3000
+    },
+    base: mode === 'production' ? '/' : '/'
+  }
 })
