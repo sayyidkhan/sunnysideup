e0b7272d2631796b2bf389cf46eaf47d23e0cced
sayyidkhan <sayyidkhan92@hotmail.com>
2024-12-05 15:59:25 +0800

    build searchbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </StrictMode>,
 )
