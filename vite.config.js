3f325b14e42931c11826a454eb5fe9a7e0ae7324
sayyidkhan <sayyidkhan92@hotmail.com>
2024-12-05 23:34:10 +0800

    vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: true,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
   },
   server: {
     port: 3000
-  }
+  },
+  base: '/'
 })
