ac89abcc66572788e2690973a4e079509b90a130
nas <nasrulhaq2010@hotmail.com>
2024-12-07 18:57:36 +0800

    fix location word background

--- src/components/detailedInsights/cards/DetailLocationCard.jsx
+++ src/components/detailedInsights/cards/DetailLocationCard.jsx
@@ -8,7 +8,7 @@ export function DetailLocationCard({ location }) {
       <div className="h-full w-full">
         <MiniMap location={location} />
         <div className="absolute bottom-3 left-3 z-[400] flex flex-col gap-1">
-          <div className="text-sm font-medium text-white bg-gray-800/30 backdrop-blur-sm px-3 py-1 rounded-lg">Location</div>
+          <div className="text-sm font-medium text-white bg-gray-800/30 backdrop-blur-sm px-3 py-1 rounded-lg w-fit">Location</div>
           <div className="text-sm font-medium text-white bg-gray-800/30 backdrop-blur-sm px-3 py-1 rounded-lg">
             {location?.lat?.toFixed(2)} N {location?.long?.toFixed(2)} E
           </div>
